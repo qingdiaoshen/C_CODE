ef5d25f895996a1797cb32d42e41ed94289f2c8b
qingdiaoshen <87431092+qingdiaoshen@users.noreply.github.com>
2021-09-27 18:22:27 +0800

    Update Guessing number games.c

--- Guessing number games/Guessing number games.c
+++ Guessing number games/Guessing number games.c
@@ -1,14 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <time.h>//牛逼
 #include <windows.h>
-//��������Ϸ
-//1. �����������һ��1-100֮�������
-//2. ��Ҳ�����
-//   ����¶��ˣ�����ʾ���¶���
-//   ����´��ˣ�����ʾ���´��ˣ����߲�С�ˣ�ֱ������ȷ
-//3. ������
+//猜数字游戏
+//1. 电脑随机生成一个1-100之间的数字
+//2. 玩家猜数字
+//   如果猜对了，就提示：猜对了
+//   如果猜错了，就提示：猜大了，或者猜小了，直到猜正确
+//3. 反复玩
 
 void menu()
 {
@@ -24,29 +24,29 @@ void game()
 	int ret = 0;
 	int guess = 0;
 	ret = rand() % 100 + 1;
-	//rand()����������������һϵ��α���ֵ,���Ǹ���һ����(����)Ϊ��׼�������صĵ��ƹ�ʽ���������һϵ����,���������ͬ����ô��������������Ҳ����ͬ�ġ�
-	//ÿһ�ε��ö���������һ������,һ�ε��û��ǵ�ǰ������.
-	//��Ҫ����#include <stdlib.h>
-	//��Ҫ����[m, n]��Χ�ڵ������num�����ã�
+	//rand()函数可以用来生成一系列伪随机值,它是根据一个数(种子)为基准按照神秘的递推公式推算出来的一系列数,如果种子相同，那么后面产生的随机数也是相同的。
+	//每一次调用都会生成下一个数字,一次调用还是当前的数字.
+	//需要引入#include <stdlib.h>
+	//如要产生[m, n]范围内的随机数num，可用：
 	//int num = rand() % (n - m + 1) + m;
 
 
-	printf("�������\n");
+	printf("请猜数字\n");
 	while (1)
 	{
 		scanf("%d", &guess);
 		getchar();
 		if (guess > ret)
 		{
-			printf("�´���\n");
+			printf("猜大了\n");
 		}
 		else if (guess < ret)
 		{
-			printf("��С��\n");
+			printf("猜小了\n");
 		}
 		else
 		{
-			printf("�¶���\n");
+			printf("猜对了\n");
 			break;
 		}
 	}
@@ -55,43 +55,43 @@ void game()
 int main()
 {
 	srand((unsigned int)time(NULL));
-	//rand()����ÿ�ε���ǰ�����ѯ�Ƿ���ù�srand(seed)���Ƿ��srand�趨��һ��ֵ������еĻ������ͻ��Զ�����srand��seed��һ������ʼ��������ʼֵ��
-	//���֮ǰû��srand��seed������ôϵͳ���Զ���seed��ֵ��ʼֵΪ1��Ȼ��rand()������һ�Ρ�
+	//rand()函数每次调用前都会查询是否调用过srand(seed)，是否给srand设定了一个值，如果有的话，它就会自动调用srand（seed）一次来初始化它的起始值。
+	//如果之前没有srand（seed），那么系统会自动给seed赋值初始值为1，然后rand()调用它一次。
 
-	//���ǳ���ʹ��ʱ�������1970��1��1��0ʱ0��0�뵽����ʱ��һ���ж����룩����ȡʱ�䡣
-	//time()����������ȡϵͳʱ�䣬����������time_t�������ͣ����ͣ�����Ĳ�����time_t*��ָ�����ͣ�
-	//time()��Ҫ����#include <time.h>��
-	//NULL�ǿ�ָ�롣
+	//我们常常使用时间戳（从1970年1月1日0时0分0秒到现在时间一共有多少秒）来获取时间。
+	//time()函数用来获取系统时间，返回类型是time_t（长整型）类型，里面的参数是time_t*（指针类型）
+	//time()需要引入#include <time.h>。
+	//NULL是空指针。
 
 
 
 	char input = 0;
 	do
 	{
-		if (input == '0')//Ϊ����ֹѭ��,������Ϸ.
+		if (input == '0')//为零终止循环,结束游戏.
 		{
 			break;
 		}
 		menu();
-		printf("��ѡ��:");
+		printf("请选择:");
 		scanf("%c", &input);
 		switch (input)
 		{
 
 		case '1':
-			game(); //��������Ϸ����
+			game(); //猜数字游戏主体
 			Sleep(4000);
 			system("cls");
 			break;
 
 		case '0':
-			printf("�˳�����\n");
+			printf("退出程序\n");
 			Sleep(1000);
 			system("cls");
 			break;
 
 		default:
-			printf("����������\n");
+			printf("请重新输入\n");
 			Sleep(1000);
 			system("cls");
 			break;
@@ -99,9 +99,9 @@ int main()
 
 
 	} while (input == '1' || input != '1');
-	//inputΪ1��ʱ����һ�ν���ѭ��ѯ������Ƿ�����Ϸ��
-	//inputΪ0��ʱ�򣬲��ٽ���ѭ������Ϸ�����ˡ�
-	//inputΪ�����ʱ�򣬽���ѭ�������������ѡ��
+	//input为1的时候，再一次进入循环询问玩家是否玩游戏。
+	//input为0的时候，不再进入循环，游戏结束了。
+	//input为非零的时候，进入循环，让玩家重新选择。
 	return 0;
 }
 
